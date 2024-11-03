1fbef7e2e84ccd11f5ab2c02a7832d4e0f94d556
Riya Singh <riya435465@gmail.com>
2024-11-03 07:22:22 +0530

    fourth change

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,35 @@
 import 'package:appflut_1/pages/home_page.dart';
 import 'package:appflut_1/pages/login_page.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   // ignore: prefer_const_constructors
-  runApp( MYapp()
-  );  //class ke bahar toh isko function bolnge
+  runApp(MYapp()); //class ke bahar toh isko function bolnge
 }
-
 
 class MYapp extends StatelessWidget {
   const MYapp({super.key});
 
-
   @override
-  Widget build(BuildContext context) { //build is a method like doing some work
+  Widget build(BuildContext context) {
+    //build is a method like doing some work
 
-    return MaterialApp(      //class ke andar function hai toh usko method bolte hai
+    return MaterialApp(
+      //class ke andar function hai toh usko method bolte hai
       // home:HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
-      initialRoute: "/home",
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: 'Arial'),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
       routes: {
-        "/" : (context) => LoginPage(),
+        "/": (context) => LoginPage(),
         // ignore: prefer_const_constructors
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage()
       },
     );
   }
-
-
-  
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,14 +8,47 @@ class LoginPage extends StatelessWidget {  //class blueprint usese jo bana woh b
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-          fontSize: 40,
-          color: Colors.blueAccent,
-          fontWeight: FontWeight.bold,
-        ),),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png",fit: BoxFit.cover,),//contain- size according compress, fill-it will fill even if it needs to strech
+          
+          // ignore: prefer_const_constructors
+          Text("Welcome",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          SizedBox(height:
+           20.0,),
+           Padding(
+             padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+             child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Enter username",
+                  labelText: "username"),
+             
+             ),
+              TextFormField(
+                obscureText: true,
+                  decoration: InputDecoration(hintText: "Enter password",
+                  labelText: "password"),
+             
+             ),
+              SizedBox(height:
+           20.0,),
+           ElevatedButton(onPressed: () {
+            print("hi welcome");
+           },
+            child: Text("Login"),
+            style: TextButton.styleFrom(),),
+
+             ],),
+           )
+        ],
       ),
     );
   }
